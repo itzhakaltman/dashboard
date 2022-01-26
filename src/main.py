d5f824599350307a118344afff77042d82874b9d
itzhakaltman <96288626+itzhakaltman@users.noreply.github.com>
2022-01-26 07:36:03 +0200

    0.0.01

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
-#!/usr/bin/env python3
+import telebot;
+bot = telebot.TeleBot('%token%');
 
-greeting = 'hello world'
+@bot.message_handler(content_types=['text', 'document', 'audio'])
+def get_text_messages(message):
+    if message.text == "Hey":
+        bot.send_message(message.from_user.id, "Hey! How are you?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Say 'Hey'!")
+    else:
+        bot.send_message(message.from_user.id, "I don't understand you, say /help.")
 
-print(greeting)
+bot.polling(none_stop=True, interval=0)
