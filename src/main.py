20fe36f3e1c50208526ebb3f503ef3ac351280e0
itzhakaltman <96288626+itzhakaltman@users.noreply.github.com>
2022-01-27 06:04:05 +0200

    0.0.02

--- src/main.py
+++ src/main.py
@@ -1,13 +1,30 @@
-import telebot;
-bot = telebot.TeleBot('%token%');
+import os
+import telebot
+from dotenv import load_dotenv
 
-@bot.message_handler(content_types=['text', 'document', 'audio'])
-def get_text_messages(message):
-    if message.text == "Hey":
-        bot.send_message(message.from_user.id, "Hey! How are you?")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Say 'Hey'!")
-    else:
-        bot.send_message(message.from_user.id, "I don't understand you, say /help.")
 
-bot.polling(none_stop=True, interval=0)
+
+
+# environment variables defined inside a .env file
+load_dotenv()
+TELEBOT_TOKEN = os.getenv('TELEBOT_TOKEN')
+
+print(TELEBOT_TOKEN)
+
+
+
+
+
+
+# bot = telebot.TeleBot('%token%');
+#
+# @bot.message_handler(content_types=['text', 'document', 'audio'])
+# def get_text_messages(message):
+#     if message.text == "Hey":
+#         bot.send_message(message.from_user.id, "Hey! How are you?")
+#     elif message.text == "/help":
+#         bot.send_message(message.from_user.id, "Say 'Hey'!")
+#     else:
+#         bot.send_message(message.from_user.id, "I don't understand you, say /help.")
+#
+# bot.polling(none_stop=True, interval=0)
