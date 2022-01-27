71e9338352880b1f1a1d5506b5cf5d7da68c093b
itzhakaltman <96288626+itzhakaltman@users.noreply.github.com>
2022-01-27 06:27:06 +0200

    0.0.03

--- src/main.py
+++ src/main.py
@@ -1,30 +1,21 @@
-import os
 import telebot
+import os
 from dotenv import load_dotenv
-
-
-
 
 # environment variables defined inside a .env file
 load_dotenv()
 TELEBOT_TOKEN = os.getenv('TELEBOT_TOKEN')
-
-print(TELEBOT_TOKEN)
-
+bot = telebot.TeleBot(TELEBOT_TOKEN)
 
 
+@bot.message_handler(content_types=['text', 'document', 'audio'])
+def get_text_messages(message):
+    if message.text == "Hey":
+        bot.send_message(message.from_user.id, "Hey! How are you?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Say 'Hey'!")
+    else:
+        bot.send_message(message.from_user.id, "I don't understand you, say /help.")
 
 
-
-# bot = telebot.TeleBot('%token%');
-#
-# @bot.message_handler(content_types=['text', 'document', 'audio'])
-# def get_text_messages(message):
-#     if message.text == "Hey":
-#         bot.send_message(message.from_user.id, "Hey! How are you?")
-#     elif message.text == "/help":
-#         bot.send_message(message.from_user.id, "Say 'Hey'!")
-#     else:
-#         bot.send_message(message.from_user.id, "I don't understand you, say /help.")
-#
-# bot.polling(none_stop=True, interval=0)
+bot.polling(none_stop=True, interval=0)
